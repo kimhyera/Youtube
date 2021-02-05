34d14ea5720fd441ae98b1866da5569a19b9efc4
kimhyera <khr1828@naver.com>
2021-02-05 14:32:53 +0900

    리팩토링

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -12,25 +12,29 @@ class Youtube {
 
 
     }
-    mostPopular() {
+    async mostPopular() {
 
-        return fetch(
-            `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-            this.getRequestOptions
-        )
-            .then(response => response.json())
-            .then(result => result.items)
-            .catch(error => console.log('error', error));
+        try {
+            const response = await fetch(
+                `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+                this.getRequestOptions
+            );
+            const result = await response.json();
+            return result.items;
+        } catch (error) {
+            return console.log('error', error);
+        }
 
     }
 
-    search(query) {
-        return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=${this.key}`, this.getRequestOptions)
-
-            .then(response => response.json())
-            .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
-
-            .catch(error => console.log('error', error));
+    async search(query) {
+        try {
+            const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=${this.key}`, this.getRequestOptions);
+            const result = await response.json();
+            return result.items.map(item => ({ ...item, id: item.id.videoId }));
+        } catch (error) {
+            return console.log('error', error);
+        }
 
     }
 }
